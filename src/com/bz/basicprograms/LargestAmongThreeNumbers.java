be6b6e87a89e84e10833f3f2d9c1287147f5cbe3
Subhamdas20 <subham21.sd@gmail.com>
2021-10-22 11:05:08 +0530

    find largest among three

---
+++ src/com/bz/basicprograms/LargestAmongThreeNumbers.java
@@ -0,0 +1,25 @@
+package com.bz.basicprograms;
+
+import java.util.Scanner;
+
+public class LargestAmongThreeNumbers {
+    public static void findLargest(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number");
+        int number1=sc.nextInt();
+        System.out.println("Enter the second number");
+        int number2=sc.nextInt();
+        System.out.println("Enter the third number");
+        int number3=sc.nextInt();
+        if((number1>number2)&(number1>number3))
+            System.out.println(number1+" is largest among three");
+        else if ((number2>number1)&(number2>number3))
+            System.out.println(number2+" is largest among three");
+        else
+            System.out.println(number3+" is largest among three");
+    }
+    public static void main(String[] args) {
+       findLargest();
+
+    }
+}
