ffc74ab41796b2cc07c373c06f5413e5be65bcd4
Subhamdas20 <subham21.sd@gmail.com>
2021-10-22 10:16:45 +0530

    Quotient And Remainder

--- src/com/bz/basicprograms/ComputingQuotientAndRemainder.java
+++ src/com/bz/basicprograms/ComputingQuotientAndRemainder.java
@@ -3,18 +3,19 @@ package com.bz.basicprograms;
 import java.util.Scanner;
 
 public class ComputingQuotientAndRemainder {
-    public void compute(){
-        Scanner sc=new Scanner(System.in);
+    public void compute() {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number ");
-        int number=sc.nextInt();
+        int number = sc.nextInt();
         System.out.println("Enter the divisor");
-        int divisor= sc.nextInt();
-        int quotient,remainder;
-        quotient=number/divisor;
-        remainder=number%divisor;
-        System.out.println("The quotient is "+quotient);
-        System.out.println("The remainder is "+ remainder);
+        int divisor = sc.nextInt();
+        int quotient, remainder;
+        quotient = number / divisor;
+        remainder = number % divisor;
+        System.out.println("The quotient is " + quotient);
+        System.out.println("The remainder is " + remainder);
     }
+
     public static void main(String[] args) {
         ComputingQuotientAndRemainder comp = new ComputingQuotientAndRemainder();
         comp.compute();
