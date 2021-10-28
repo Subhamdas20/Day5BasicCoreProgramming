b96fa6623fd184d616c0803018cee76ac9b5f96d
Subhamdas20 <subham21.sd@gmail.com>
2021-10-28 23:18:06 +0530

    Added : uc13 to calculate distance

--- src/com/bz/basicprograms/Distance.java
+++ src/com/bz/basicprograms/Distance.java
@@ -2,8 +2,8 @@ package com.bz.basicprograms;
 
 import java.util.Scanner;
 
-public class
-Distance {
+public class Distance {
+    /* Added method to calculate distance  */
     static void  calculateEuclideanDistance(int x,int y){
         double distance=Math.sqrt(Math.pow(x,2)+Math.pow(y,2));
         System.out.println("Euclidean distance of point (x,y) to the origin (0,0) is = "+distance);
